23ec2deaf391705ecaf95f5f08eb62f746cecf11
Kacper Zelichowski <kafejek@gmail.com>
2017-04-26 10:07:43 +0200

    Added an add function

--- gittie.py
+++ gittie.py
@@ -1,1 +1,9 @@
-print("Hello, I'm Gittie!".")
+print("Hello, I'm Gittie!")
+
+
+def add(li):
+    '''This function adds stuff'''
+    li_2 = []
+    for element in li:
+        li_2.append(element*2)
+    return li_2
